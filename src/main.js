f98fff54d41d841325322d6dbd188f8a95159fd0
tj1250850539 <1250850539@qq.com>
2019-04-14 17:48:19 +0800

    '项目初始化'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import store from './store/index'
 
-Vue.config.productionTip = false
+Vue.use(Vant)
 
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  //放数据的
+  state: {
+
+  },
+  //对数据的二次计算
+  getters: {
+
+  },
+  //唯一能修改state 的东西
+  mutations: {
+
+  },
+  //可以多次调用mutations的东西
+  actions: {
+
+  }
+})
+
+export default store
